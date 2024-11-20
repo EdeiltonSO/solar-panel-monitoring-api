4d6eeaf8cdfc349d6bf7ff14084d7adeb279d0c2
Edeilton Oliveira <edeiltonso@gmail.com>
2024-11-20 05:13:46 -0300

    test: add crud pro repositório em memória de usuários

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,5 +1,6 @@
 import { User, Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository-interface'
+import { UserNotFoundError } from '@/services/errors/user-not-found-error'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
@@ -24,10 +25,75 @@ export class InMemoryUsersRepository implements UsersRepository {
       password_hash: data.password_hash,
       verified_at: null,
       created_at: new Date(),
+      maintenance_code: null,
+      maintenance_code_created_at: null
     }
 
     this.items.push(user)
 
     return user
+  }
+
+  async update(id: string, data: Prisma.UserUpdateInput) {
+    const userIndex = this.items.findIndex((item) => item.id === id);
+
+    if (userIndex === -1) {
+        throw new UserNotFoundError();
+    }
+
+    const user = this.items[userIndex];
+
+    const updatedUser = {
+        ...user,
+        ...data,
+    };
+
+    this.items[userIndex] = updatedUser as User;
+
+    return updatedUser as User;
+  }
+
+  async validate(id: string) {
+    const user = this.items.find((item) => item.id === id);
+
+    if (!user) {
+        throw new UserNotFoundError();
+    }
+
+    user.verified_at = new Date();
+    return { verified_at: user.verified_at };
+  }
+
+  async changePassword(id: string, newPasswordHash: string) {
+    const user = this.items.find((item) => item.id === id);
+
+    if (!user) {
+        throw new UserNotFoundError();
+    }
+
+    user.password_hash = newPasswordHash;
+
+    return user;
+  }
+
+  async generateMaintenanceCode(id: string, maintenanceCode: string) {
+    const user = this.items.find((item) => item.id === id);
+
+    if (!user) {
+        throw new UserNotFoundError();
+    }
+
+    user.maintenance_code = maintenanceCode;
+    user.maintenance_code_created_at = new Date();
+  }
+
+  async remove(id: string) {
+    const userIndex = this.items.findIndex((item) => item.id === id);
+
+    if (userIndex === -1) {
+        throw new UserNotFoundError();
+    }
+
+    this.items.splice(userIndex, 1);
   }
 }
