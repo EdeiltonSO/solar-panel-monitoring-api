a8974af8867990827dfce44dbb665a42a25ab01b
Edeilton Oliveira <edeiltonso@gmail.com>
2024-11-29 05:03:04 -0300

    fix: add propriedade sub no tipo do jwt

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -3,6 +3,7 @@ import '@fastify/jwt'
 declare module '@fastify/jwt' {
   export interface FastifyJWT {
     user: {
+      sub: string
       id: string
       email: string
       validated_at: string | null
